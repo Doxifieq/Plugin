24197b86774aaad0ef5f3c55cd6675e38b3cdf5f
BuildTools <unconfigured@null.spigotmc.org>
2022-12-01 08:40:45 +0100

    update

--- src/main/java/core/core/commands/heal.java
+++ src/main/java/core/core/commands/heal.java
@@ -7,8 +7,6 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.util.Objects;
-
 public class heal implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String string, String[] args) {
