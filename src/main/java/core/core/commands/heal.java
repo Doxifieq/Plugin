feb98d2d52177de365919878f8de3cd185a32d52
BuildTools <unconfigured@null.spigotmc.org>
2022-12-01 08:42:58 +0100

    update

--- src/main/java/core/core/commands/heal.java
+++ src/main/java/core/core/commands/heal.java
@@ -7,13 +7,15 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.Objects;
+
 public class heal implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String string, String[] args) {
         if(sender instanceof Player && sender.isOp()) {
             Player player = (Player) sender;
 
-            player.setHealth(player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue());
+            player.setHealth(Objects.requireNonNull(player.getAttribute(Attribute.GENERIC_MAX_HEALTH)).getValue());
             player.sendMessage(ChatColor.YELLOW + "You have been healed.");
 
             return true;
